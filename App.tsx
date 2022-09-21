7ff762efb86d94515360e3bae12d79bead5fb660
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-21 20:31:59 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import './style.css';
+import { useState, useEffect } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  useEffect(() => {
+    console.log(name.length);
+    if (name == "sahi hai") setIsValid(true);
+    else setIsValid(false);
+  }, [name]);
+  function nameChangeHandler(event) {
+    console.log("yaha bhi change hua");
+    if (event.target.value.trim().length >= 5) setName("sahi hai");
+    else setName("galat baat");
+  }
+  return (
+    <div className="parent">
+      <div>{name}</div>
+      <div>
+        <form>
+          <div className={isValid ? "child" : "wrong"}>
+            <label>Name : </label>
+            <input className="ip" onChange={nameChangeHandler}></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-rupk6y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-rupk6y)
